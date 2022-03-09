8c372b63b934bb8aeca80a6a28d6459217e90b72
mobssie <mobssie.jung@gmail.com>
2022-03-09 22:03:32 +0900

    react init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/about/About';
+import Home from './pages/home/Home';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
+
+export default App;
