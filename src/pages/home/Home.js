2bfe87d0d14558cee88f9e6b523595a363f0871d
mobssie <mobssie.jung@gmail.com>
2022-03-18 05:05:39 +0900

    Home 만들기

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import About from '../about/About';
+import DataTable from '../../components/DataTable'
 import styled from 'styled-components';
-<script src="https://cdn.jsdelivr.net/npm/drive-db"></script>
+
+const Button = styled.button`
+  background: ${props => props.primary ? "#db7093" : "#fff"};
+  color: ${props => props.primary ? "#fff" : "#db7093"};
+
+  position: absolute;
+  right: 8px;
+  font-size: 1em;
+  padding: 4px 6px;
+  border: 2px solid #db7093;
+  border-radius: 3px;
+  line-height: 1;
+`;
 
 const Home = () => {
-    return (
-        <div>
-            <h2></h2>
-            <About/>
-            <input type="text"/>
+  return (
+    <div>
+      <h2>배당금</h2>
+      <div className="warp_top">
+        <div className="unit_data">
+          <span className="tit_basic info_date">날짜</span>
+          <input type="text" className="inp_basic inp_date"/>
         </div>
-    );
+        <div className="unit_data">
+          <span className="tit_basic info_date">종목명</span>
+          <input type="text" className="inp_basic inp_date"/>
+        </div>
+        <div className="unit_data">
+          <span className="tit_basic info_date">배당금</span>
+          <input type="text" className="inp_basic inp_date"/>
+        </div>
+        <Button primary>추가</Button>
+      </div>
+      <DataTable/>
+    </div>
+  );
 };
 
 export default Home;
