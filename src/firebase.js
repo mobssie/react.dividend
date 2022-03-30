2b135d9fff106bbb9f7488d7e7d646b5d071d236
mobssie <mobssie.jung@gmail.com>
2022-03-30 22:56:08 +0900

    firebase getAuth, onAuthStateChanged

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import firebase from "firebase/compat/app"
 import 'firebase/compat/firestore';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -15,7 +17,20 @@ firebase.initializeApp(config);
 
 // firebase의 firestore 인스턴스를 변수에 저장
 const firestore = firebase.firestore();
-
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  console.log('user', auth);
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
 // 필요한 곳에서 사용할 수 있도록 내보내기
 // 다른 곳에서 불러올때 firestore로 불러와야 함!!
 export { firestore };
+
